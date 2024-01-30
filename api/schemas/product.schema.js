c002cb2b34e327a573cdc7caa5e3a6e277a74169
ivan andres <andresgom11012@gmail.com>
2024-01-29 22:53:09 -0500

    Update product.schema.js

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,16 +1,18 @@
 const Joi = require('joi');
 
-const id = Joi.string().uuid();
+const id = Joi.number().integer();
 const name = Joi.string().min(3).max(15);
 const description = Joi.string().min(10)
 const price = Joi.number().integer().min(10);
-const image = Joi.string().uri()
+const image = Joi.string().uri();
+const categoryId = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
   description: description.required(),
   price: price.required(),
-  image: image.required()
+  image: image.required(),
+  categoryId: categoryId.required()
 })
 
 const updateProductSchema = Joi.object({
