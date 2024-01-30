8bfbe9f5f7f450f8e8f7208b7f1faff3c4be609b
ivan andres <andresgom11012@gmail.com>
2024-01-29 23:37:14 -0500

    feat: adds order, model, migration, servies, and order-product

--- api/db/models/index.js
+++ api/db/models/index.js
@@ -2,6 +2,8 @@ const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
 const { Category, CategorySchema } = require('./category.model');
 const { Product, ProductSchema } = require('./product.model');
+const { Order, OrderSchema } = require('./order.model');
+const { OrderProduct, OrderProductSchema } = require('./order-product.model');
 
 
 function setupModels(sequelize) {
@@ -9,11 +11,14 @@ function setupModels(sequelize) {
   Customer.init(CustomerSchema, Customer.config(sequelize));
   Category.init(CategorySchema, Category.config(sequelize));
   Product.init(ProductSchema, Product.config(sequelize));
+  Order.init(OrderSchema, Order.config(sequelize));
+  OrderProduct.init(OrderProductSchema, OrderProduct.config(sequelize));
 
   Customer.associate(sequelize.models)
   User.associate(sequelize.models)
   Category.associate(sequelize.models)
   Product.associate(sequelize.models)
+  Order.associate(sequelize.models);
 }
 
 module.exports = setupModels;
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,7 @@ const productsRouter = require('./products.js')
 const userRouter = require('./users.js')
 const categoriesRouter = require('./categories.js')
 const customerRoutes = require('./customers.router.js')
+const orderRouter = require('./orders.router');
 
 const routerApi = (app) => {
   const router = express.Router();
@@ -11,6 +12,7 @@ const routerApi = (app) => {
   router.use('/products', productsRouter);
   router.use('/users', userRouter);
   router.use('/categories', categoriesRouter);
+  router.use('/orders', orderRouter);
   router.use('/customer', customerRoutes);
   // End Api version 1
 }
