48fe4fa1c7809c528635dd002963e5bd87283294
ivan andres <andresgom11012@gmail.com>
2024-01-30 23:04:51 -0500

    Update index.js

--- api/config/index.js
+++ api/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.DB_PORT || 3000,
+  port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
